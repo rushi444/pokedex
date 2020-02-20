276223de8510b29a5a0cf5951f7ddd1cb21e6a20
rushi444 <rushi44@gmail.com>
2020-02-20 10:42:39 -0800

    build out fetch call

--- pokedex/app.js
+++ pokedex/app.js
@@ -1,1 +1,18 @@
-console.log('blueface baby')
+const fetchPokemon = async () => {
+  const promises = [];
+  for (let i = 1; i <= 150; i++) {
+    const url = `https://pokeapi.co/api/v2/pokemon/${i}`;
+    const res = await (await fetch(url)).json();
+    promises.push(res);
+  }
+  const allResponses = Promise.all(promises);
+  const pokemon = (await allResponses).map(data => ({
+    name: data.name,
+    id: data.id,
+    image: data.sprites['front_default'],
+    type: data.types.map(type => type.type.name).join(', '),
+  }));
+  console.log(pokemon);
+};
+
+fetchPokemon();
