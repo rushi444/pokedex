271a20b8ce69fdf16ada659521e40b2f68007ef4
rushi444 <rushi44@gmail.com>
2020-02-20 11:46:36 -0800

    All list items rendering and added css effects

--- pokedex/app.js
+++ pokedex/app.js
@@ -1,3 +1,6 @@
+const pokedex = document.getElementById('pokedex');
+console.log(pokedex);
+
 const fetchPokemon = async () => {
   const promises = [];
   for (let i = 1; i <= 150; i++) {
@@ -12,7 +15,22 @@ const fetchPokemon = async () => {
     image: data.sprites['front_default'],
     type: data.types.map(type => type.type.name).join(', '),
   }));
+  displayPokemon(pokemon);
+};
+
+const displayPokemon = pokemon => {
   console.log(pokemon);
+  const pokemonHTMLString = pokemon
+    .map(
+      eachPokemon => `
+  <li class='card'>
+  <img class='card-image' src="${eachPokemon.image}">
+  <h2 class='card-title'>${eachPokemon.id}. ${eachPokemon.name}</h2>
+  <p class='card-subtitle'>Type: ${eachPokemon.type}</p>
+  </li>`,
+    )
+    .join('');
+  pokedex.innerHTML = pokemonHTMLString;
 };
 
 fetchPokemon();
